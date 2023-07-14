029f124cdd1e81138f0c4f5dbc3257bc1b6cebeb
Homework <allamurodovravshan38@gmail.com>
2023-07-14 11:13:07 +0500

    scroll view added..

---
+++ lib/ui/widgets/test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class TEst extends StatelessWidget {
+  const TEst({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
